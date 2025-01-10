f8479ae535e836b46f4c4d2204ee0aa650ac70f1
niranois13 <8784@holbertonstudents.com>
2025-01-10 12:03:54 +0100

    Added task0

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -10,4 +10,32 @@
 * Return: the adress of the inserted node or NULL on failure.
 */
 
-listint_t *insert_node(listint_t **head, int number);
+listint_t *insert_node(listint_t **head, int number) {
+    listint_t *temp, *new_node;
+    temp = *head;
+
+    if (head == NULL) {
+        return (NULL);
+    }
+
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL) {
+        free(new_node);
+        return (NULL);
+    }
+
+    if (*head == NULL || (*head)->n >= number) {
+        new_node->next = *head;
+        *head = new_node;
+        return (new_node);
+    }
+
+    while (temp->next != NULL && temp->next->n > number) {
+        temp = temp->next;
+    }
+
+    new_node->next = temp->next;
+    temp->next = new_node;
+
+    return (new_node);
+}
