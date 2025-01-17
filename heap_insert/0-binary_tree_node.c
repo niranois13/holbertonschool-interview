ffc7b29295036d529d46c78a35cd2117eaa13b30
niranois13 <8784@holbertonstudents.com>
2025-01-17 15:00:19 +0100

    Adding Heap insert task0

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -22,17 +22,17 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->right = NULL;
 	new_node->parent = parent;
 
-	if (parent == NULL)
-		return (new_node);
-	else if (parent->left == NULL)
-		parent->left = new_node;
-	else if (parent->right == NULL)
-		parent->right = new_node;
-	else
-	{
-		free(new_node);
-		return (NULL);
-	}
+	// if (parent == NULL)
+	// 	return (new_node);
+	// else if (parent->left == NULL)
+	// 	parent->left = new_node;
+	// else if (parent->right == NULL)
+	// 	parent->right = new_node;
+	// else
+	// {
+	// 	free(new_node);
+	// 	return (NULL);
+	// }
 
 	return (new_node);
 }
