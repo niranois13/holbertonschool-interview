38d1745d5ff6201ac6c6a9632884509527bc965b
niranois13 <8784@holbertonstudents.com>
2025-05-16 10:36:12 +0200

    Task 0

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -4,52 +4,64 @@ Count it!
 """
 
 import requests
-import sys
+import re
 
 
-def count_words(subreddit, word_list, kw_cont={}, next_pg=None, reap_kw={}):
-    """all hot posts by keyword"""
-    headers = {"User-Agent": "nildiert"}
+def count_words(subreddit, word_list, kw_cont=None, after=None):
+    """Recursively count keywords in titles of hot posts"""
+    if kw_cont is None:
+        kw_cont = {}
+        for word in word_list:
+            lw = word.lower()
+            if lw in kw_cont:
+                kw_cont[lw] += 1
+            else:
+                kw_cont[lw] = 1
 
-    if next_pg:
-        subr = requests.get('https://reddit.com/r/' + subreddit +
-                            '/hot.json?after=' + next_pg, headers=headers)
-    else:
-        subr = requests.get('https://reddit.com/r/' + subreddit +
-                            '/hot.json', headers=headers)
+        return count_words(subreddit, word_list, {k: 0 for k in kw_cont}, None)
 
-    if subr.status_code == 404:
+    headers = {'User-Agent': 'custom'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    if after:
+        url += f'?after={after}'
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return
 
-    if kw_cont == {}:
-        for word in word_list:
-            kw_cont[word] = 0
-            reap_kw[word] = word_list.count(word)
+    data = response.json().get('data', {})
+    posts = data.get('children', [])
+    after = data.get('after')
 
-    subr_dict = subr.json()
-    subr_data = subr_dict['data']
-    next_pg = subr_data['after']
-    subr_posts = subr_data['children']
+    def process_post(index):
+        if index >= len(posts):
+            return
+        title = posts[index].get('data', {}).get('title', '').lower()
+        words = re.findall(r'\b\w+\b', title)
 
-    for post in subr_posts:
-        post_data = post['data']
-        post_title = post_data['title']
-        title_words = post_title.split()
-        for w in title_words:
-            for key in kw_cont:
-                if w.lower() == key.lower():
-                    kw_cont[key] += 1
+        def count_word(w_idx):
+            if w_idx >= len(words):
+                return
+            word = words[w_idx]
+            if word in kw_cont:
+                kw_cont[word] += 1
+            count_word(w_idx + 1)
 
-    if next_pg:
-        count_words(subreddit, word_list, kw_cont, next_pg, reap_kw)
+        count_word(0)
+        process_post(index + 1)
 
-    else:
-        for key, val in reap_kw.items():
-            if val > 1:
-                kw_cont[key] *= val
+    process_post(0)
 
-        sorted_abc = sorted(kw_cont.items(), key=lambda x: x[0])
-        sorted_res = sorted(sorted_abc, key=lambda x: (-x[1], x[0]))
-        for res in sorted_res:
-            if res[1] > 0:
-                print('{}: {}'.format(res[0], res[1]))
+    if after:
+        return count_words(subreddit, word_list, kw_cont, after)
+
+    def sort_and_print(items, index=0):
+        if index >= len(items):
+            return
+        key, count = items[index]
+        if count > 0:
+            print(f"{key}: {count}")
+        sort_and_print(items, index + 1)
+
+    sorted_items = sorted(kw_cont.items(), key=lambda x: (-x[1], x[0]))
+    sort_and_print(sorted_items)
