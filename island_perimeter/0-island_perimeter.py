e1da9194d6a250bf358a292fefa3bd8ec2d9693b
niranois13 <8784@holbertonstudents.com>
2025-07-15 14:31:28 +0200

    Task 0

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+0-island_perimeter
+"""
+
+
+def island_perimeter(grid):
+    perimeter = 0
+
+    if not grid or len(grid) == 0:
+        return 0
+
+    for i in range(len(grid) - 1):
+        for j in range(len(grid[i]) - 1):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if grid[i][j+1] == 1:
+                    perimeter -= 1
+                if grid[i][j-1] == 1:
+                    perimeter -= 1
+                if grid[i-1][j] == 1:
+                    perimeter -= 1
+                if grid[i+1][j] == 1:
+                    perimeter -= 1
+    return perimeter
