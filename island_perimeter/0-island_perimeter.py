7001a46d1b38be741c79a4bbc48e8e2dc0f49840
niranois13 <8784@holbertonstudents.com>
2025-07-15 14:51:22 +0200

    Task 0

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -7,20 +7,22 @@
 def island_perimeter(grid):
     """Finds perimeter of an island"""
     perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
 
     if not grid or len(grid) == 0:
         return 0
 
-    for i in range(len(grid) - 1):
-        for j in range(len(grid[i]) - 1):
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4
-                if grid[i][j+1] == 1:
+                if i > 0 and grid[i-1][j] == 1:
                     perimeter -= 1
-                if grid[i][j-1] == 1:
+                if i < rows - 1 and grid[i+1][j] == 1:
                     perimeter -= 1
-                if grid[i-1][j] == 1:
+                if j > 0 and grid[i][j-1] == 1:
                     perimeter -= 1
-                if grid[i+1][j] == 1:
+                if j < cols - 1 and grid[i][j+1] == 1:
                     perimeter -= 1
     return perimeter
