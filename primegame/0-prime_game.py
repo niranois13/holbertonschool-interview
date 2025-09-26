f94d398f52ad1f2718ab8e677aca1768a0c70c7f
niranois13 <8784@holbertonstudents.com>
2025-09-26 11:40:24 +0200

    Updated Task 0

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -1,64 +1,50 @@
 #!/usr/bin/python3
 """ Prime Game """
 
-
-def isprime(n):
-    """ n: number to check if it is prime"""
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
-
-
-def delete_numbers(n, nums):
-    """ delete numbers - assign zero """
-    for i in range(len(nums)):
-        if nums[i] % n == 0:
-            nums[i] = 0
-
+def sieve(max_n):
+    """ Return a list indicating if numbers <= max_n are prime """
+    is_prime = [False, False] + [True] * (max_n - 1)
+    for i in range(2, int(max_n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, max_n + 1, i):
+                is_prime[j] = False
+    return is_prime
 
 def isWinner(x, nums):
-    """ where x is the number of rounds and nums is an array of n
-        Return: name of the player that won the most rounds
-        Iriaf the winner cannot be determined, return None
-        You can assume n and x will not be larger than 10000
     """
-    nums.sort()
-    winner = False
-    Maria = 0
-    Ben = 0
-    for game in range(x):
-        # print("game# ", game+1)
-        nums2 = list(range(1, nums[game] + 1))
-        # print("nums: ", nums2)
-        turn = 0
-        while True:
-            """
-            # uncomment to monitor turns
-            if turn % 2 != 0:
-                print("Ben turn ")
-            else:
-                print("Maria turn ")
-            """
-            change = False
-            for i, n in enumerate(nums2):
-                # print("n: ", n, "i: ", i)
-                if n > 1 and isprime(n):
-                    delete_numbers(n, nums2)
-                    change = True
-                    turn += 1
-                    break
-            # print("movement: ", nums2)
-            if change is False:
-                break
-        if turn % 2 != 0:
-            Maria += 1
-        else:
-            Ben += 1
-        # print("Maria: {}, Ben: {}".format(Maria, Ben))
-    if Maria == Ben:
-        return None
-    if Maria > Ben:
-        return "Maria"
-    return "Ben"
+    Determines the winner of the Prime Game.
     
+    Args:
+        x (int): number of rounds
+        nums (list): list of n values for each round
+    
+    Returns:
+        str: "Maria" if Maria wins more rounds,
+            "Ben" if Ben wins more rounds,
+            None if no winner can be determined
+    """
+    if x == 0 or not nums:
+        return None
+
+    max_n = max(nums)
+    primes = sieve(max_n)
+
+    # Precompute number of primes ≤ n for all n
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if primes[i] else 0)
+
+    Maria_wins = 0
+    Ben_wins = 0
+
+    for n in nums:
+        if prime_count[n] % 2 != 0:
+            Maria_wins += 1
+        else:
+            Ben_wins += 1
+
+    if Maria_wins > Ben_wins:
+        return "Maria"
+    if Ben_wins > Maria_wins:
+        return "Ben"
+    return None
